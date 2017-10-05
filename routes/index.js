03b5a9ff57df0c6c75f1ea5ce83fbec22f1804d2
Karl OConnor <karl67@gmail.com>
2017-10-05 15:43:12 +0100

    put back

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ module.exports = function(app, addon) {
 
 	});
 
-	app.get('/installed', function(req, res) {
+	app.get('/installed', addon.authenticate(), function(req, res) {
 		res.status(200);
 	});
 };
