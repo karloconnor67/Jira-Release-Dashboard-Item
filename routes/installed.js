01f6ace8a990108bbdf4ec19aab09748ecace088
Karl OConnor <karl67@gmail.com>
2017-10-05 14:08:52 +0100

    add res status

--- routes/installed.js
+++ routes/installed.js
@@ -1,9 +1,10 @@
 module.exports = function(app, addon) {
 
 	app.get(function(req, res) {
-		res.render('installed', {
-			title : "Installed"
-		});
+		// res.render('installed', {
+		// title : "Installed"
+		// });
+		res.status(200);
 
 	});
 };
